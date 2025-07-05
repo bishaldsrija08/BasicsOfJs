e1600edc1b0bcb7683d339ce9b185094b1afeb70
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-07-05 13:45:44 +0545

    array and objects done

--- inServer/array3.js
+++ inServer/array3.js
@@ -2,7 +2,6 @@
 //slice, splice, and split
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-
 // Slice returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included).
 // The original array will not be modified.
 // The start and end arguments represent the index of the array.
