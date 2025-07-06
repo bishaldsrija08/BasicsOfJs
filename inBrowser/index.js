89baf06fc8c544edd713b26e39ff2f973fca0961
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-07-06 22:12:11 +0545

    DOM complete

--- inBrowser/index.js
+++ inBrowser/index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+//Learning DOM Manipulation
+
+const hello = document.querySelector(".thisIsH1")
+hello.innerHTML = "<Mark>Document Object hello</Mark>"
+
+
+const button = document.getElementsByTagName("button")[0]
+button.textContent = "Change helloo"
+
+
+button.addEventListener("click", () => {
+    hello.textContent = "Color Changed"
+})
+
+button.addEventListener("click", () => {
+    var newPara = document.createElement("h1")
+    newPara.textContent = "New Paragraph Added"
+    document.body.appendChild(newPara)
+})
+
